cc8d2bd0928f9ffc30430bf20b41257bc45266e0
Mert18 <mertuygur02@gmail.com>
2021-11-22 01:10:06 +0300

    heyo

--- next.config.js
+++ next.config.js
@@ -6,5 +6,6 @@ module.exports = {
     GET_ADMINS: process.env.GET_ADMINS,
     GET_MESSAGES: process.env.GET_MESSAGES,
     CREATE_MESSAGE: process.env.CREATE_MESSAGE,
+    RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -17,31 +17,19 @@ export default function Home({ events, admins, fileNames }) {
   const [email, setEmail] = useState("");
   const [title, setTitle] = useState("");
   const [message, setMessage] = useState("");
-
-  const recaptchaRef = React.createRef();
+  const [disabled, setDisabled] = useState(true);
 
   const handleChangeImage = (e) => {
     setMainImage(e.target.alt);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
-    recaptchaRef.current.execute();
-  };
-
-  const onReCAPTCHAChange = async (captchaCode) => {
-    // If the reCAPTCHA code is null or undefined indicating that
-    // the reCAPTCHA was expired then return early
-    if (!captchaCode) {
-      return;
-    }
     try {
       const data = {
         title: title,
         email: email,
         message: message,
-        captcha: captchaCode,
       };
       await fetch(`${publicRuntimeConfig.CREATE_MESSAGE}`, {
         method: "POST",
@@ -50,28 +38,19 @@ export default function Home({ events, admins, fileNames }) {
         },
         body: JSON.stringify(data),
       })
-        .then((response) => {
-          if (response.ok) {
-            // If the response is ok than show the success alert
-            alert("Mesaj gönderildi.");
-          } else {
-            // Else throw an error with the message returned
-            // from the API
-            const error = response.json();
-            throw new Error(error.message);
-          }
-        })
+        .then((response) => response.json())
         .then((res) => console.log(res))
         .catch((err) => console.log("Errro!", err));
     } catch (error) {
       alert(error?.message || "Something went wrong");
     } finally {
-      // Reset the reCAPTCHA when the request has failed or succeeeded
-      // so that it can be executed again if user submits another email.
-      recaptchaRef.current.reset();
       setTitle("");
       setEmail("");
     }
+  };
+
+  const onReCAPTCHAChange = () => {
+    setDisabled(false);
   };
 
   return (
@@ -162,9 +141,7 @@ export default function Home({ events, admins, fileNames }) {
           </div>
           <form onSubmit={handleSubmit}>
             <ReCAPTCHA
-              ref={recaptchaRef}
-              size="invisible"
-              sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
+              sitekey="6Lelr00dAAAAABsGdIrh6bkyhPnMoYibu0r6SRBt"
               onChange={onReCAPTCHAChange}
             />
 
@@ -190,7 +167,9 @@ export default function Home({ events, admins, fileNames }) {
               <label>Konu</label>
               <input type="text" onChange={(e) => setTitle(e.target.value)} />
             </div>
-            <button type="submit">Gönder</button>
+            <button type="submit" disabled={disabled}>
+              Gönder
+            </button>
           </form>
         </section>
       </div>
