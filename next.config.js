005565fc7976fda009442ffd921cbddd204af14b
Mert18 <mertuygur02@gmail.com>
2022-01-17 18:02:00 +0300

    ae

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     GET_MESSAGES: process.env.GET_MESSAGES,
     CREATE_MESSAGE: process.env.CREATE_MESSAGE,
     UPDATE_MESSAGE: process.env.UPDATE_MESSAGE,
-    RECAPTCHA_SITE_KEY: proces.env.RECAPTCHA_SITE_KEY,
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
     RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
     PASS: process.env.PASS,
   },
